20d31f43e32aed81ec4d0d9eaf9acb28c3ee4393
Xghayor <cc.baqir@gmail.com>
2024-01-10 20:08:08 +0000

    fix linters

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  has_many :posts, foreign_key: :author_id 
-  
+  has_many :posts, foreign_key: :author_id
+
 
   def three_recent_posts
     posts.order(created_at: :desc).limit(3)
