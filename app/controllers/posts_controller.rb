75ae0858c4188ae50735fdbd33bcbe61affaeb62
Xghayor <cc.baqir@gmail.com>
2024-01-15 17:20:30 +0000

    Fix linters

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,18 +1,17 @@
 class PostsController < ApplicationController
-    before_action :set_user
-  
-    def index
-      @posts = @user.posts.all
-    end
-  
-    def show
-      @post = @user.posts.find(params[:id])
-    end
-  
-    private
-  
-    def set_user
-      @user = User.find(params[:user_id])
-    end
+  before_action :set_user
+
+  def index
+    @posts = @user.posts.all
   end
-  
+
+  def show
+    @post = @user.posts.find(params[:id])
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:user_id])
+  end
+end
