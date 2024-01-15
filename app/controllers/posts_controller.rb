b8cb796a886aeeb7bb0618dbf031ec7e939a3aeb
Xghayor <cc.baqir@gmail.com>
2024-01-15 17:17:00 +0000

    Add controllers and views for models

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,18 @@
+class PostsController < ApplicationController
+    before_action :set_user
+  
+    def index
+      @posts = @user.posts.all
+    end
+  
+    def show
+      @post = @user.posts.find(params[:id])
+    end
+  
+    private
+  
+    def set_user
+      @user = User.find(params[:user_id])
+    end
+  end
+  
