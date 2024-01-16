35d6ccf7850283ab5843730a492223f73229dabd
Xghayor <cc.baqir@gmail.com>
2024-01-16 17:57:51 +0000

    fix linters

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,24 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
-    let(:user) do
-        User.create!(name: 'John', photo: 'example.jpg', bio: 'Some bio', posts_counter: 0)
-      end
-      
-      describe 'GET /index' do
-        it 'renders a successful response, correct template and includes correct placeholder text' do
-          post = user.posts.create!(title: 'Example Post', text: 'This is an example post', comments_counter: 0, likes_counter: 0)
-          get user_posts_path(user)
-          expect(response).to be_successful
-          expect(response).to render_template(:index)
-          expect(response.body).to include(post.title)
-        end
-      end
-      
+  let(:user) do
+    User.create!(name: 'John', photo: 'example.jpg', bio: 'Some bio', posts_counter: 0)
+  end
+
+  describe 'GET /index' do
+    it 'renders a successful response, correct template and includes correct placeholder text' do
+      post = user.posts.create!(title: 'Example Post', text: 'This is an example post', comments_counter: 0,
+                                likes_counter: 0)
+      get user_posts_path(user)
+      expect(response).to be_successful
+      expect(response).to render_template(:index)
+      expect(response.body).to include(post.title)
+    end
+  end
+
 
   describe 'GET /show' do
     let(:post) do
-      user.posts.create!(title: 'Example Post', text: 'This is an example post', comments_counter: 0, likes_counter: 0)
+      user.posts.create!(title: 'Example Post', text: 'This is an example post', comments_counter: 0,
+                         likes_counter: 0)
     end
 
     it 'renders a successful response, correct template and includes correct placeholder text' do
