73ac2e2c73e9859685d0488ad4d5592599ab4c2b
Xghayor <cc.baqir@gmail.com>
2024-01-17 19:28:40 +0000

    add forms for CRUD

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+    before_action :current_user
+
+    def current_user
+        @user = User.find(1)
+    end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,8 @@
 class PostsController < ApplicationController
-  before_action :set_user
+
+  def new
+    @post = Post.new
+  end
 
   def index
     @posts = @user.posts
@@ -9,10 +12,20 @@ class PostsController < ApplicationController
     @post = @user.posts.find(params[:id])
   end
 
+  def create
+    @post = current_user.posts.create(post_params)
+    @post.update(comments_counter:0, likes_counter: 0)
+    if @post.save
+      redirect_to user_posts_path(current_user)
+    else
+      render :new
+    end
+  end
+  
+
   private
 
-  def set_user
-    @user = User.find_by(id: params[:user_id])
-    redirect_to root_path, alert: 'User not found' unless @user
+  def post_params
+    params.require(:post).permit(:title, :text)
   end
 end
