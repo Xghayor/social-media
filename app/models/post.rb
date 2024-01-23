3ccc6afe644242909cbba2cf811b72357597881f
Xghayor <cc.baqir@gmail.com>
2024-01-23 17:10:42 +0100

    Resolve N+1 problem

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,8 @@
 class Post < ApplicationRecord
-  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+  belongs_to :author, class_name: 'User'
   has_many :comments
   has_many :likes
+
 
   validates :title, presence: true, length: { maximum: 250 }
   validates_numericality_of :comments_counter, only_integer: true, greater_than_or_equal_to: 0
