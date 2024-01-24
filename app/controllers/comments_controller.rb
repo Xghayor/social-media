d6a9764dc05a22a7f325f9555a0e05e0b2de1337
Xghayor <cc.baqir@gmail.com>
2024-01-24 21:47:23 +0100

    fix linters

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,16 +1,16 @@
 class CommentsController < ApplicationController
-  before_action :set_post
+  before_action :set_post_and_user
 
   def new
     @comment = Comment.new
   end
 
   def create
-    @comment = @post.comments.create(comment_params)
+    @comment = @post.comments.build(comment_params)
     @comment.user = current_user
 
     if @comment.save
-      redirect_to user_posts_path(@post.author)
+      redirect_to user_posts_path(@user)
     else
       render :new
     end
@@ -18,7 +18,8 @@ class CommentsController < ApplicationController
 
   private
 
-  def set_post
+  def set_post_and_user
+    @user = User.find(params[:user_id])
     @post = Post.find(params[:post_id])
   end
 
