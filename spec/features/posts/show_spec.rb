abe93dc012d73c5c0cbbdf55b9f59fefd6e36589
Xghayor <cc.baqir@gmail.com>
2024-01-24 10:19:37 +0100

    modify show_spec.rb

--- spec/features/posts/show_spec.rb
+++ spec/features/posts/show_spec.rb
@@ -13,42 +13,50 @@ RSpec.describe 'Post', type: :feature do
   describe 'Show page' do
     it "should display the post's title" do
       visit user_post_path(@user1, @post1)
+      
       expect(page).to have_content(@post1.title)
     end
 
     it "should display the post's author" do
       visit user_post_path(@user1, @post1)
+
       expect(page).to have_content(@user1.name)
     end
 
     it "should display the post's number of comments" do
       visit user_post_path(@user1, @post1)
+
       expect(page).to have_content(@post1.comments_counter)
     end
 
     it "should display the post's number of likes" do
       visit user_post_path(@user1, @post1)
+
       expect(page).to have_content(@post1.likes_counter)
     end
 
     it "should display the post's body" do
       visit user_post_path(@user1, @post1)
+
       expect(page).to have_content(@post1.text)
     end
 
     it 'should display the username of each commentator' do
       visit user_post_path(@user1, @post1)
+
       expect(page).to have_content(@user1.name)
     end
 
     it 'should display the text of each comment' do
       visit user_post_path(@user1, @post1)
+
       expect(page).to have_content(@comment1.text)
       expect(page).to have_content(@comment2.text)
     end
 
     it "Should display the 'Back to Posts' button" do
       visit user_post_path(@user1, @post1)
+
       expect(page).to have_link('Back to Posts')
     end
   end
